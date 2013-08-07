b124b38fe8fefd45b0833080288fdc70e09cf7b8
Renan Augusto Dembogurski <ad.renan@gmail.com>
2013-08-06 21:23:03 -0300

    Changed completely the download.sh file

--- bin/download.sh
+++ bin/download.sh
@@ -5,14 +5,22 @@
 #+------------------------------------------------------------------------------------------------------------------------------+
 PROGNAME=$(basename $0)
 
-#Config parameters (adjust according your target language and folder)
+##### Config parameters (adjust according your target language and folder)
+
 export lang_i18n=pt
+export comp_languages=(en bg ca cs de el es fr hu it ko pl ru sl tr)
 export language=portuguese
 export dbpedia_workspace=/var/local/spotlight
 export dbpedia_version=3.8
-export complement_types=0
+export comp_types=0
+DIRECTORY_PREFIX=$dbpedia_workspace + "/dbpedia_data/original/dbpedia/"
+OPENNLP_PREFIX="http://opennlp.sourceforge.net/models-1.5/"
 
-# error_exit function by William Shotts. http://stackoverflow.com/questions/64786/error-handling-in-bash
+#+------------------------------------------------------------------------------------------------------------------------------+
+#| Functions                                                                                                                    |
+#+------------------------------------------------------------------------------------------------------------------------------+
+
+# Error_exit function by William Shotts. http://stackoverflow.com/questions/64786/error-handling-in-bash
 function error_exit
 {
     echo "${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
@@ -20,184 +28,172 @@ function error_exit
 }
 touch $dbpedia_workspace/foo && rm -f $dbpedia_workspace/foo || error_exit "ERROR: The directory '$dbpedia_workspace' is not writable! Change its permissions or choose another 'dbpedia_workspace' in download.sh"
 
-set -e
-# Creating directories
-echo 'Creating directories...'
-if [ -e $dbpedia_workspace/dbpedia_data ]; then 
-    echo "$dbpedia_workspace"'/dbpedia_data/ already exists.'
-else
-    mkdir $dbpedia_workspace/dbpedia_data
-fi
+# The function used to create all the directories needed
+function create_dir()
+{
+    if [ -e $1 ]; then
+        echo $1 'already exists.'
+    else
+        mkdir $1
+    fi
+}
 
-if [ -e $dbpedia_workspace/dbpedia_data/original ]; then 
-    echo "$dbpedia_workspace"'/dbpedia_data/original already exists.'
-else
-    mkdir $dbpedia_workspace/dbpedia_data/original
-fi
+# The function used to test the languages used as complement to the initial language
+function test_comp_lang()
+{
+    local arr=$1
+    # Loop through the languages array
+    for i in ${arr[@]}
+    do
+       # Checking if the complement languages supplied are valid
+       if [ $(expr length $i) -ne 2 ] | [ "$i" == "$lang" ] | `expr match "$i" '[A-Za-z]'` -ne 2 ]
+           error_exit
+    done
+}
 
-if [ -e $dbpedia_workspace/dbpedia_data/original/wikipedia/  ]; then 
-    echo "$dbpedia_workspace"'/dbpedia_data/original/wikipedia/ already exists.'
-else
-    mkdir $dbpedia_workspace/dbpedia_data/original/wikipedia/
-fi
+# Just some initial processing for the type complement task
+init
+{
+    # We will need the instance types file in the .nt format so we can create a TDB store using it. For that
+    # we unzip the .bz2 file related to the initial language
+    bunzip2 $DIRECTORY_PREFIX$lang + /instance_types_$lang_i18n.nt.bz2
 
-if [ -e $dbpedia_workspace/dbpedia_data/original/wikipedia/$lang_i18n  ]; then 
-    echo "$dbpedia_workspace"'/dbpedia_data/original/wikipedia/'"$lang_i18n"' already exists.'
-else
-    mkdir $dbpedia_workspace/dbpedia_data/original/wikipedia/$lang_i18n
-fi
+    # We also need to download the bijective interlanguage links file of the initial language.
+    download_file http://downloads.dbpedia.org/$dbpedia_version/$lang_i18n/interlanguage_links_same_as_$lang_i18n.nt.bz2
+}
 
-if [ -e $dbpedia_workspace/dbpedia_data/original/dbpedia  ]; then 
-    echo "$dbpedia_workspace"'/dbpedia_data/original/dbpedia already exists.'
-else
-    mkdir $dbpedia_workspace/dbpedia_data/original/dbpedia
-fi
+# Just a helper function to download files from a given path. The first parameter is the path from where to download the file
+# and the second states the language, which is useful when dealing with multiple languages
+download_file()
+{
+    # Only downloads if there is no current file or there is a newer version
+    case "$#" in {
+        "1")
+            wget -N $1
+            ;;
+        "2")
+            wget -N $1 --directory-prefix=$DIRECTORY_PREFIX$2
+            ;;
+        *)
+            error_exit; }
+    echo 'done!'
+}
 
-if [ -e $dbpedia_workspace/dbpedia_data/original/dbpedia/$lang_i18n  ]; then 
-    echo "$dbpedia_workspace"'/dbpedia_data/original/dbpedia/'"$lang_i18n"'already exists.'
-else
-    mkdir $dbpedia_workspace/dbpedia_data/original/dbpedia/$lang_i18n
-fi
+# A slightly modified version of the download function. This time if we don't find the initial language files
+# we change the path to get the english OpenNLP files
+dl_opennlp_file()
+{
+    local file=$1'-'$2".bin"
+    local path=$OPENNLP_PREFIX$file
+    local new_path=""
+    local string_to_replace=""
+    wget -q --spider $path
+    if [ $? -eq 0 ] ; then
+        wget -N $path
+    else
+        echo "$file not found. Getting an English version..."
+        string_to_replace=$1'-'
+        new_path=$(echo $path | sed -e s/"$string_to_replace"/en-/g)
+        echo $new_path
+        wget -q --spider $new_path
+        if [ $? -eq 0 ] ; then
+            wget -N $new_path
+        else
+            string_to_replace="en-"
+            new_path=$(echo $new_path | sed -e s/"$string_to_replace"/en-ner-/g)
+            wget -N $new_path
+        fi
+    fi
+}
 
-if [ -e $dbpedia_workspace/dbpedia_data/data  ]; then 
-    echo "$dbpedia_workspace"'/data already exists.'
-else
-    mkdir $dbpedia_workspace/dbpedia_data/data
-fi
-
-
-if [ -e $dbpedia_workspace/dbpedia_data/data/output  ]; then
-    echo "$dbpedia_workspace"'/data/output already exists.'
-else
-    mkdir $dbpedia_workspace/dbpedia_data/data/output
-fi
-
-if [ -e $dbpedia_workspace/dbpedia_data/data/opennlp  ]; then 
-    echo "$dbpedia_workspace"'/data/opennlp already exists.'
-else
-    mkdir $dbpedia_workspace/dbpedia_data/data/opennlp
-fi
-
-if [ -e $dbpedia_workspace/dbpedia_data/data/opennlp/$language  ]; then 
-    echo "$dbpedia_workspace"'/data/opennlp already exists.'
-else
-    mkdir $dbpedia_workspace/dbpedia_data/data/opennlp/$language
-fi
-
-if [ -e $lang != "en" ]; then
-    echo "Do you want to complement your language instance types file? the new file will be used in the indexing stage."
+# The function used in case the user wants to complement the instance types triples file
+function complement_types()
+{
+    echo "Do you want to complement your language instance types file? the new file will be used in the indexing stage (optional)."
     select yn in "Yes" "No"; do
         case $yn in
-            Yes ) if [ -e $dbpedia_workspace/dbpedia_data/original/dbpedia/en  ]; then
-                      echo "$dbpedia_workspace"'/dbpedia_data/original/dbpedia/'"en"'already exists.'
-                  else
-                      mkdir $dbpedia_workspace/dbpedia_data/original/dbpedia/en
-                      $complement_types=1
-                  fi
+            # Test if the complement languages array is empty or not defined
+            Yes ) test_comp_lang [ test $comp_languages:-$(error_exit) ]
+                  init_complement
+                  # Now for each complement language defined above we create the respective directories and download all the needed
+                  # instance types triples files.
+                  for i in ${comp_languages[@]}
+                  do
+                      # Create the base directory for each language used as complement
+                      if [ -e $DIRECTORY_PREFIX$i ]; then
+                          echo $DIRECTORY_PREFIX$i + ' already exists.'
+                      else
+                          create_dir $DIRECTORY_PREFIX$i
+                      fi
+                      # Create the TDB directory related to each language used as complement
+                      if [ -e $DIRECTORY_PREFIX$i ]; then
+                          echo $DIRECTORY_PREFIX$i + ' already exists.'
+                      else
+                          create_dir $DIRECTORY_PREFIX$i + '/TDB/' + $i
+                      fi
+                      mkdir $dbpedia_workspace/dbpedia_data/original/dbpedia/$lang_i18n/TDB/$lang_i18n
+                      download_file http://downloads.dbpedia.org/$dbpedia_version/$i/instance_types_$i.nt.bz2 $i
+                      bunzip2 $DIRECTORY_PREFIX$i/instance_types_$i.nt.bz2
+                  done
+                  $comp_types=1
                   break;;
             No ) break;;
         esac
     done
-fi
+}
 
+#+------------------------------------------------------------------------------------------------------------------------------+
+#| Main                                                                                                                         |
+#+------------------------------------------------------------------------------------------------------------------------------+
+set -e
+
+# Creating all the directories needed
+echo 'Creating directories...'
+create_dir $dbpedia_workspace/dbpedia_data
+create_dir $dbpedia_workspace/dbpedia_data/original
+create_dir $dbpedia_workspace/dbpedia_data/original/wikipedia/
+create_dir $dbpedia_workspace/dbpedia_data/original/wikipedia/$lang_i18n
+create_dir $dbpedia_workspace/dbpedia_data/original/dbpedia
+create_dir $dbpedia_workspace/dbpedia_data/original/dbpedia/$lang_i18n
+create_dir $dbpedia_workspace/dbpedia_data/data
+create_dir $dbpedia_workspace/dbpedia_data/data/output
+create_dir $dbpedia_workspace/dbpedia_data/data/opennlp
+create_dir $dbpedia_workspace/dbpedia_data/data/opennlp/$language
+
+# After creating the directories we can ask the user if he wants to complement the instance types triples file.
+# The idea is to improve the indexing of a language using other languages. This is optional.
+complement_types $lang
+
+# The next step is to download all the needed files.
 set +e
 
-
 echo 'Getting DBpedia Files...'
-wget http://downloads.dbpedia.org/$dbpedia_version/$lang_i18n/labels_$lang_i18n.nt.bz2 --directory-prefix=$dbpedia_workspace/dbpedia_data/original/dbpedia/$lang_i18n
-wget http://downloads.dbpedia.org/$dbpedia_version/$lang_i18n/redirects_$lang_i18n.nt.bz2 --directory-prefix=$dbpedia_workspace/dbpedia_data/original/dbpedia/$lang_i18n
-wget http://downloads.dbpedia.org/$dbpedia_version/$lang_i18n/disambiguations_$lang_i18n.nt.bz2 --directory-prefix=$dbpedia_workspace/dbpedia_data/original/dbpedia/$lang_i18n
-wget http://downloads.dbpedia.org/$dbpedia_version/$lang_i18n/instance_types_$lang_i18n.nt.bz2 --directory-prefix=$dbpedia_workspace/dbpedia_data/original/dbpedia/$lang_i18n
-
-if [ -e $complement_types == 1 ]; then
-    wget http://downloads.dbpedia.org/$dbpedia_version/$lang_i18n/interlanguage_links_same_as_$lang_i18n.nt.bz2 --directory-prefix=$dbpedia_workspace/dbpedia_data/original/dbpedia/$lang_i18n
-    wget http://downloads.dbpedia.org/$dbpedia_version/$lang_i18n/instance_types_en.nt.bz2 --directory-prefix=$dbpedia_workspace/dbpedia_data/original/dbpedia/en
-    bzcat $dbpedia_workspace/dbpedia_data/original/dbpedia/en/instance_types_en.nt.bz2 > $dbpedia_workspace/dbpedia_data/original/dbpedia/en/instance_types_en.nt
-    bzcat $dbpedia_workspace/dbpedia_data/original/dbpedia/$lang_i18n/instance_types_$lang_i18n.nt.bz2 > $dbpedia_workspace/dbpedia_data/original/dbpedia/$lang_i18n/instance_types_$lang_i18n.nt
-    bzcat $dbpedia_workspace/dbpedia_data/original/dbpedia/$lang_i18n/interlanguage_links_same_as_$lang_i18n.nt.bz2 > $dbpedia_workspace/dbpedia_data/original/dbpedia/$lang_i18n/interlanguage_links_same_as_$lang_i18n.nt
-    mkdir $dbpedia_workspace/dbpedia_data/original/dbpedia/$lang_i18n/TDB/$lang_i18n
-    mkdir $dbpedia_workspace/dbpedia_data/original/dbpedia/$lang_i18n/TDB/en
-fi
-
-echo 'done!'
+download_file http://downloads.dbpedia.org/$dbpedia_version/$lang_i18n/labels_$lang_i18n.nt.bz2 $lang_i18n
+download_file http://downloads.dbpedia.org/$dbpedia_version/$lang_i18n/redirects_$lang_i18n.nt.bz2 $lang_i18n
+download_file http://downloads.dbpedia.org/$dbpedia_version/$lang_i18n/disambiguations_$lang_i18n.nt.bz2 $lang_i18n
+download_file http://downloads.dbpedia.org/$dbpedia_version/$lang_i18n/instance_types_$lang_i18n.nt.bz2 $lang_i18n
 
 echo 'Getting Wikipedia Dump...'
-wget "http://dumps.wikimedia.org/"$lang_i18n"wiki/latest/"$lang_i18n"wiki-latest-pages-articles.xml.bz2" --directory-prefix=$dbpedia_workspace/dbpedia_data/original/wikipedia/$lang_i18n
-echo 'done!'
+download_file "http://dumps.wikimedia.org/"$lang_i18n"wiki/latest/"$lang_i18n"wiki-latest-pages-articles.xml.bz2" $lang_i18n
 
 echo 'Getting LingPipe Spotter...'
-wget http://dbp-spotlight.svn.sourceforge.net/viewvc/dbp-spotlight/tags/release-0.5/dist/src/deb/control/data/usr/share/dbpedia-spotlight/spotter.dict
-echo 'done!'
+download_file http://dbp-spotlight.svn.sourceforge.net/viewvc/dbp-spotlight/tags/release-0.5/dist/src/deb/control/data/usr/share/dbpedia-spotlight/spotter.dict
 
 echo 'Getting Spot Selector...'
-wget http://spotlight.dbpedia.org/download/release-0.5/spot_selector.tgz
-echo 'done!'
+download_file http://spotlight.dbpedia.org/download/release-0.5/spot_selector.tgz
 
-echo 'Getting Index...'
-wget http://dbp-spotlight.svn.sourceforge.net/viewvc/dbp-spotlight/tags/release-0.5/dist/src/deb/control/data/usr/share/dbpedia-spotlight/index.tgz
-echo 'done!'
-
-echo 'Getting Index...'
-wget http://dbp-spotlight.svn.sourceforge.net/viewvc/dbp-spotlight/tags/release-0.5/dist/src/deb/control/data/usr/share/dbpedia-spotlight/pos-en-general-brown.HiddenMarkovModel
-echo 'done!'
+echo 'Getting Index Files...'
+download_file http://dbp-spotlight.svn.sourceforge.net/viewvc/dbp-spotlight/tags/release-0.5/dist/src/deb/control/data/usr/share/dbpedia-spotlight/index.tgz
+download_file http://dbp-spotlight.svn.sourceforge.net/viewvc/dbp-spotlight/tags/release-0.5/dist/src/deb/control/data/usr/share/dbpedia-spotlight/pos-en-general-brown.HiddenMarkovModel
 
 echo 'Getting Apache OpenNLP models...'
-wget -q --spider http://opennlp.sourceforge.net/models-1.5/$lang_i18n-chunker.bin
-if [ $? -eq 0 ] ; then
-   wget http://opennlp.sourceforge.net/models-1.5/$lang_i18n-chunker.bin
-else
-   echo "$lang_i18n"'-chunker.bin not found. Getting an English version...'
-   wget http://opennlp.sourceforge.net/models-1.5/en-chunker.bin -O $lang_i18n-chunker.bin
-fi
-
-wget -q --spider http://opennlp.sourceforge.net/models-1.5/$lang_i18n-ner-location.bin
-if [ $? -eq 0 ] ; then
-   wget http://opennlp.sourceforge.net/models-1.5/$lang_i18n-ner-location.bin
-else
-   echo "$lang_i18n"'-ner-location.bin not found. Getting an English version...'
-   wget http://opennlp.sourceforge.net/models-1.5/en-ner-location.bin -O $lang_i18n-ner-location.bin
-fi
-
-wget -q --spider http://opennlp.sourceforge.net/models-1.5/$lang_i18n-ner-organization.bin
-if [ $? -eq 0 ] ; then
-   wget http://opennlp.sourceforge.net/models-1.5/$lang_i18n-ner-organization.bin
-else
-   echo "$lang_i18n"'-ner-organization.bin not found. Getting an English version...'
-   wget http://opennlp.sourceforge.net/models-1.5/en-ner-organization.bin -O $lang_i18n-ner-organization.bin
-fi
-
-wget -q --spider http://opennlp.sourceforge.net/models-1.5/$lang_i18n-ner-person.bin
-if [ $? -eq 0 ] ; then
-   wget http://opennlp.sourceforge.net/models-1.5/$lang_i18n-ner-person.bin
-else
-   echo "$lang_i18n"'-ner-person.bin not found. Getting an English version...'
-   wget http://opennlp.sourceforge.net/models-1.5/en-ner-person.bin -O $lang_i18n-ner-person.bin
-fi
-
-wget  -q --spider http://opennlp.sourceforge.net/models-1.5/$lang_i18n-pos-maxent.bin
-if [ $? -eq 0 ] ; then
-   wget http://opennlp.sourceforge.net/models-1.5/$lang_i18n-pos-maxent.bin
-else
-   echo "$lang_i18n"'-ner-pos-maxent.bin not found. Getting an English version...'
-   wget http://opennlp.sourceforge.net/models-1.5/en-pos-maxent.bin -O $lang_i18n-pos-maxent.bin
-fi
-
-wget  -q --spider http://opennlp.sourceforge.net/models-1.5/$lang_i18n-sent.bin
-if [ $? -eq 0 ] ; then
-   wget http://opennlp.sourceforge.net/models-1.5/$lang_i18n-sent.bin
-else
-   echo "$lang_i18n"'-sent.bin not found. Getting an English version...'
-   wget http://opennlp.sourceforge.net/models-1.5/en-sent.bin -O $lang_i18n-sent.bin
-fi
-
-wget  -q --spider http://opennlp.sourceforge.net/models-1.5/$lang_i18n-token.bin
-if [ $? -eq 0 ] ; then
-   wget http://opennlp.sourceforge.net/models-1.5/$lang_i18n-token.bin
-else
-   echo "$lang_i18n"'-token.bin not found. Getting an English version...'
-   wget http://opennlp.sourceforge.net/models-1.5/en-token.bin -O $lang_i18n-token.bin
-fi
-
+dl_opennlp_file $lang_i18n "chunker"
+dl_opennlp_file $lang_i18n "location"
+dl_opennlp_file $lang_i18n "organization"
+dl_opennlp_file $lang_i18n "person"
+dl_opennlp_file $lang_i18n "pos-maxent"
+dl_opennlp_file $lang_i18n "sent"
+dl_opennlp_file $lang_i18n "token"
 echo 'done!'
 
 #------------------------------------- Runtime Files --------------------------------------------------
